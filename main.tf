be6195eafd150860043604dce3a94b098be47f9d
Raj Poluri <rpoluri@expedia.com>
2018-06-15 09:22:11 -0500

    fixes #6,#7 federate multiple apiary instances in same account

--- main.tf
+++ main.tf
@@ -68,13 +68,22 @@ data "template_file" "server_yaml" {
   }
 }
 
+data "template_file" "primary_metastore_whitelist" {
+  count    = "${length(var.primary_metastore_whitelist)}"
+  template = <<EOF
+  - ${var.primary_metastore_whitelist[count.index]}
+EOF
+}
+
 data "template_file" "federation_yaml" {
   template = "${file("${path.module}/templates/waggle-dance-federation.yml.tmpl")}"
 
   vars {
     primary_metastore_host = "${var.primary_metastore_host}"
     primary_metastore_port = "${var.primary_metastore_port}"
-    fedrated_metastores    = "${join("",data.template_file.remote_metastores_yaml.*.rendered)}"
+    primary_metastore_whitelist = "${join("",data.template_file.primary_metastore_whitelist.*.rendered)}"
+    local_metastores       = "${join("",data.template_file.local_metastores_yaml.*.rendered)}"
+    remote_metastores      = "${join("",data.template_file.remote_metastores_yaml.*.rendered)}"
   }
 }
 
