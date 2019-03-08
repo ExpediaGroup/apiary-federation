36a910a9483a585f27d6fce6583be7e202830ca3
Mike Roark <msroark-comm@pobox.com>
2019-03-08 21:57:51 +0000

    Added error message and exit 1 for dns_name returned as null

--- scripts/endpoint_dns_name.sh
+++ scripts/endpoint_dns_name.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 dns_name=`aws ec2 describe-vpc-endpoints --vpc-endpoint-ids $1|jq -r ".VpcEndpoints[].DnsEntries[0].DnsName"`
+if [ "$dns_name" = "null" ]; then
+  echo dns_name is null, you may need to update your aws cli 1>&2
+  exit 1
+fi
 jq -n --arg dnsname "$dns_name" '{"dnsname":$dnsname}'
