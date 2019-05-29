c83c7d1c186697060104511ceb090acf621fbcd7
Mike Roark <msroark-comm@pobox.com>
2019-05-29 14:05:02 -0500

    Added debug flag to endpoint_dns_name.sh

--- scripts/endpoint_dns_name.sh
+++ scripts/endpoint_dns_name.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh -x
 dns_name=`aws ec2 describe-vpc-endpoints --vpc-endpoint-ids $1|jq -r ".VpcEndpoints[].DnsEntries[0].DnsName"`
 if [ "$dns_name" = "null" ]; then
   echo "dns_name for VPC endpoint is null, one issue which may cause this is an older version of the AWS CLI on the machine you run Terraform from." 1>&2
