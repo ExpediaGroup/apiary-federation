68b3cbee1ce275600424e7dac2e8cb17a7608868
Raj Poluri <rpoluri@expediagroup.com>
2019-12-11 18:00:53 -0600

    make waggle_dance_load_balancers output optional

--- ouputs.tf
+++ ouputs.tf
@@ -1,3 +1,3 @@
 output "waggle_dance_load_balancers" {
-  value = var.wd_instance_type == "k8s" ? kubernetes_service.waggle_dance[0].load_balancer_ingress.*.hostname : ""
+  value = var.wd_instance_type == "k8s" ? kubernetes_service.waggle_dance[0].load_balancer_ingress.*.hostname : []
 }
