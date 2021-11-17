09094d83b795e56712c12f4835a04c959e16b55a
Raj Poluri <rpoluri@expediagroup.com>
2021-11-16 18:19:53 -0600

    fix config aliases

--- version.tf
+++ version.tf
@@ -5,7 +5,7 @@
  */
 
 terraform {
-  required_version = "> 0.12.0, < 0.14.0"
+  required_version = "> 0.12.0, < 1.0.0"
   required_providers {
     aws = {
       source                = "hashicorp/aws"
