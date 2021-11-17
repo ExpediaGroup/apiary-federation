1f2045cd7c032677118f725ff0e3f957a9a51009
Raj Poluri <rpoluri@expediagroup.com>
2021-11-16 18:08:45 -0600

    test configuration_aliases

--- version.tf
+++ version.tf
@@ -6,4 +6,11 @@
 
 terraform {
   required_version = "> 0.12.0, < 0.14.0"
+  required_providers {
+    aws = {
+      source                = "hashicorp/aws"
+      version               = ">= 2.7.0"
+      configuration_aliases = [aws.remote]
+    }
+  }
 }
