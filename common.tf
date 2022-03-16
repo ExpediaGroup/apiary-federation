a7bd89fef6c355e0a8558e6a0bfab12d88fee5d9
Scott Barnhart <sbarnhart@expedia.com>
2022-03-15 17:57:44 -0700

    iam fixes

--- common.tf
+++ common.tf
@@ -24,3 +24,27 @@ data "aws_secretsmanager_secret" "docker_registry" {
   count = var.docker_registry_auth_secret_name == "" ? 0 : 1
   name  = var.docker_registry_auth_secret_name
 }
+
+
+data "aws_iam_policy_document" "waggle_dance_glue_policy" {
+  count = length(var.glue_metastores) > 0 ? 1 : 0
+  statement {
+    sid = "WaggledanceGluePolicy"
+    actions = [
+      "glue:GetDatabase",
+      "glue:GetDatabases",
+      "glue:GetTable",
+      "glue:GetTables",
+      "glue:GetTableVersions",
+      "glue:GetPartition",
+      "glue:GetPartitions",
+      "glue:BatchGetPartition",
+      "glue:GetUserDefinedFunction",
+      "glue:GetUserDefinedFunctions"
+    ]
+    resources = [
+    for glue_metastore in var.glue_metastores:
+    format("arn:aws:glue:%s:%s:*", var.aws_region, glue_metastore["glue-account-id"])
+    ]
+  }
+}
