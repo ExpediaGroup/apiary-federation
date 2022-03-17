0d2fdab4de04fecaf9cb3c1be5d6d33086bb9f8b
Raj Poluri <rpoluri@expediagroup.com>
2022-03-17 17:43:33 -0500

    update sg cidr to fix healthcheck

--- sg.tf
+++ sg.tf
@@ -27,7 +27,7 @@ resource "aws_security_group" "wd_sg" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = [data.aws_vpc.waggledance_vpc.cidr_block]
+    cidr_blocks = data.aws_vpc.waggledance_vpc.cidr_block_associations.*.cidr_block
   }
 
   egress {
