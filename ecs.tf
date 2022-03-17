365a3064d308e53d8a6de91e6e56e088374ff6d7
Raj Poluri <rpoluri@expediagroup.com>
2022-03-17 11:31:43 -0500

    fix

--- ecs.tf
+++ ecs.tf
@@ -24,7 +24,7 @@ resource "aws_ecs_service" "waggledance_service" {
   }
 
   dynamic "service_registries" {
-    for_each = var.enable_autscaling ? [] : ["1"]
+    for_each = var.enable_autoscaling ? [] : ["1"]
     content {
       registry_arn = var.enable_autoscaling ? null : aws_service_discovery_service.metastore_proxy[0].arn
     }
