29c5ff068059add6783e217084a6554740c94e51
patduin <pduin@expediagroup.com>
2022-03-31 16:51:27 +0200

    enabled service discover when autoscaling to fix prometheus scraping

--- ecs-service-discovery.tf
+++ ecs-service-discovery.tf
@@ -4,14 +4,20 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  */
 
+ locals {
+ //avoiding name clash with LB entry when autoscaling
+ //need both names for prometheus scraping
+   dns_name = var.enable_autoscaling ? "${local.instance_alias}-${var.aws_region}.${var.domain_extension}-sd" : ${local.instance_alias}-${var.aws_region}.${var.domain_extension}"
+ }
+
 resource "aws_service_discovery_private_dns_namespace" "waggledance" {
-  count = var.wd_instance_type == "ecs" && var.enable_autoscaling == false ? 1 : 0
-  name  = "${local.instance_alias}-${var.aws_region}.${var.domain_extension}"
+  count = var.wd_instance_type == "ecs" ? 1 : 0
+  name  = local.dns_name
   vpc   = var.vpc_id
 }
 
 resource "aws_service_discovery_service" "metastore_proxy" {
-  count = var.wd_instance_type == "ecs" && var.enable_autoscaling == false ? 1 : 0
+  count = var.wd_instance_type == "ecs" ? 1 : 0
   name  = "metastore-proxy"
 
   dns_config {
@@ -31,7 +37,7 @@ resource "aws_service_discovery_service" "metastore_proxy" {
 }
 
 resource "aws_route53_zone_association" "secondary" {
-  count      = var.wd_instance_type == "ecs" && var.enable_autoscaling == false ? length(var.secondary_vpcs) : 0
+  count      = var.wd_instance_type == "ecs" ? length(var.secondary_vpcs) : 0
   zone_id    = aws_service_discovery_private_dns_namespace.waggledance[0].hosted_zone
   vpc_id     = var.secondary_vpcs[count.index]
   vpc_region = var.aws_region
