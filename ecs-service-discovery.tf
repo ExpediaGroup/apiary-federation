64c08a556e15354ab2eb6e041437084d0cf09900
Scott Barnhart <sbarnhart@expedia.com>
2022-04-08 19:58:19 -0700

    fix zone attempt2

--- ecs-service-discovery.tf
+++ ecs-service-discovery.tf
@@ -5,7 +5,7 @@
  */
 
 resource "aws_service_discovery_private_dns_namespace" "waggledance" {
-  count = var.wd_instance_type == "ecs" && !var.enable_autoscaling ? 1 : 0
+  count = var.wd_instance_type == "ecs" && var.enable_autoscaling == false ? 1 : 0
   name  = "${local.instance_alias}-${var.aws_region}.${var.domain_extension}"
   vpc   = var.vpc_id
 }
@@ -15,18 +15,19 @@ resource "aws_service_discovery_service" "metastore_proxy" {
   name  = "metastore-proxy"
 
   dns_config {
+    # This line does not work if we have autoscaling enabled.
     namespace_id = var.enable_autoscaling ? aws_route53_zone.waggledance[0].id : aws_service_discovery_private_dns_namespace.waggledance[0].id
 
     # We always want SRV records, but we only want A records if we are not auto-scaling.
     # If we are auto-scaling, we have an ELB that manages the instances.
     dns_records {
-      ttl = 10
+      ttl  = 10
       type = "SRV"
     }
     dynamic "dns_records" {
       for_each = var.enable_autoscaling ? [] : ["1"]
       content {
-        ttl = 10
+        ttl  = 10
         type = "A"
       }
     }
