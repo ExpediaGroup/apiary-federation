59c5054f7103948e99b65b957c5f3fe75b6dcbdb
rpoluri <38321430+rpoluri@users.noreply.github.com>
2022-07-20 11:39:12 -0500

    Add waggle-dance load balancer output when using ECS and autoscaling (#110)
    * ecs lb output
    
    * update changelog
    
    Co-authored-by: Raj Poluri <rpoluri@expediagroup.com>

--- ouputs.tf
+++ ouputs.tf
@@ -1,3 +1,3 @@
 output "waggle_dance_load_balancers" {
-  value = var.wd_instance_type == "k8s" ? kubernetes_service.waggle_dance[0].load_balancer_ingress.*.hostname : []
+  value = var.wd_instance_type == "k8s" ? kubernetes_service.waggle_dance[0].load_balancer_ingress.*.hostname : (var.wd_instance_type == "ecs" && var.enable_autoscaling ? [aws_lb.waggledance[0].dns_name] : [])
 }
