a9c90270092fbafab6f67218c38601b3dace48a4
githubjianli <51385385+githubjianli@users.noreply.github.com>
2023-05-05 11:40:41 -0700

    Patch 6 (#119)
    * change output to array
    
    * change output to list

--- ouputs.tf
+++ ouputs.tf
@@ -1,3 +1,3 @@
 output "waggle_dance_load_balancers" {
-  value = var.wd_instance_type == "k8s" ? kubernetes_service.waggle_dance[0].status.0.load_balancer.0.ingress.0.hostname : (var.wd_instance_type == "ecs" && var.enable_autoscaling ? [aws_lb.waggledance[0].dns_name] : [])
+  value = var.wd_instance_type == "k8s" ? [kubernetes_service.waggle_dance[0].status.0.load_balancer.0.ingress.0.hostname] : (var.wd_instance_type == "ecs" && var.enable_autoscaling ? [aws_lb.waggledance[0].dns_name] : [])
 }
