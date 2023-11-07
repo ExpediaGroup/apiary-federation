676ac290c8de6a24fe2bdb55abb9cadab878e18c
paduin <a-paduin@expediagroup.com>
2023-11-07 13:46:41 +0100

    testing

--- ecs.tf
+++ ecs.tf
@@ -113,8 +113,6 @@ resource "aws_lb_target_group" "waggledance" {
     protocol = "HTTP"
     port     = 18000
     path     = "/actuator/health/liveness"
-    interval = 30
-    timeout  = 10
   }
 
   lifecycle {
