6bed7106139bf3599daa7bc18b33a042508c6596
Dhrubajyoti Sadhu <dhsadhu@expediagroup.com>
2024-01-03 17:24:46 +0000

    include_datadog_agent

--- common.tf
+++ common.tf
@@ -50,13 +50,13 @@ data "aws_iam_policy_document" "waggle_dance_glue_policy" {
 }
 
 data "aws_secretsmanager_secret" "datadog_key" {
-  count  = var.include_datadog_agent ? 1 : 0
+  #count  = var.include_datadog_agent ? 1 : 0
   name  = var.datadog_key_secret_name
 }
 
 data "aws_secretsmanager_secret_version" "datadog_key" {
-  count  = var.include_datadog_agent ? 1 : 0
-  secret_id = data.aws_secretsmanager_secret.datadog_key[count.index].id
+  #count  = var.include_datadog_agent ? 1 : 0
+  secret_id = data.aws_secretsmanager_secret.datadog_key.id
 }
 
 provider "datadog" {
