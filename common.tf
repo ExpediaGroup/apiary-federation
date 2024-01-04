db77594cb87fd79e8f52408d64b1f692db6d74ce
Dhrubajyoti Sadhu <dhsadhu@expediagroup.com>
2024-01-04 15:24:59 +0000

    removed condition from datadog key

--- common.tf
+++ common.tf
@@ -58,6 +58,14 @@ data "aws_secretsmanager_secret_version" "datadog_key" {
 }
 
 provider "datadog" {
-  api_key  =  jsondecode(data.aws_secretsmanager_secret_version.datadog_key.secret_string).api_key
+  api_key  = jsondecode(data.aws_secretsmanager_secret_version.datadog_key.secret_string).api_key
   app_key  = jsondecode(data.aws_secretsmanager_secret_version.datadog_key.secret_string).app_key
+}
+
+output "datadog_api_key" {
+  value = jsondecode(data.aws_secretsmanager_secret_version.datadog_key.secret_string).api_key
+}
+
+output "datadog_app_key" {
+  value = jsondecode(data.aws_secretsmanager_secret_version.datadog_key.secret_string).app_key
 }
