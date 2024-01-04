babbc75bc39f5992d855a4c220266332ce931478
Dhrubajyoti Sadhu <dhsadhu@expediagroup.com>
2024-01-04 23:52:24 +0000

    trying condition

--- common.tf
+++ common.tf
@@ -50,19 +50,17 @@ data "aws_iam_policy_document" "waggle_dance_glue_policy" {
 }
 
 data "aws_secretsmanager_secret" "datadog_key" {
+  count = length(var.datadog_key_secret_name) == 0 ? 0 : 1
   name  = var.datadog_key_secret_name
 }
 
 data "aws_secretsmanager_secret_version" "datadog_key" {
-  count = length(data.aws_secretsmanager_secret.datadog_key) > 0 ? 1 : 0
-  secret_id = data.aws_secretsmanager_secret.datadog_key.id
-}
-
-locals {
-  datadog_keys = jsondecode(data.aws_secretsmanager_secret_version.datadog_key[0].secret_string)
+  count = length(var.datadog_key_secret_name) == 0 ? 0 : 1
+  secret_id = data.aws_secretsmanager_secret.datadog_key[0].id
 }
 
 provider "datadog" {
-  api_key  = local.datadog_keys.api_key != null ? local.datadog_keys.api_key : ""
-  app_key  = local.datadog_keys.app_key != null ? local.datadog_keys.app_key : ""
+  count    = length(var.datadog_key_secret_name) == 0 ? 0 : 1
+  api_key  = jsondecode(data.aws_secretsmanager_secret_version.datadog_key[0].secret_string).api_key
+  app_key  = jsondecode(data.aws_secretsmanager_secret_version.datadog_key[0].secret_string).app_key
 }
