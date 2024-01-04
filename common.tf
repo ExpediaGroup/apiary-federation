672709edcd6daac5cc2f6e32518cc1d82d56490d
Dhrubajyoti Sadhu <dhsadhu@expediagroup.com>
2024-01-04 17:17:07 +0000

    trying condition

--- common.tf
+++ common.tf
@@ -60,6 +60,6 @@ data "aws_secretsmanager_secret_version" "datadog_key" {
 }
 
 provider "datadog" {
-  api_key  = jsondecode(data.aws_secretsmanager_secret_version.datadog_key.secret_string).api_key
-  app_key  = jsondecode(data.aws_secretsmanager_secret_version.datadog_key.secret_string).app_key
+  api_key  = jsondecode(data.aws_secretsmanager_secret_version.datadog_key[0].secret_string).api_key
+  app_key  = jsondecode(data.aws_secretsmanager_secret_version.datadog_key[0].secret_string).app_key
 }
