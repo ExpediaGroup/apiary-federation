2514ee01b0aa3e2a3bf706192a74d975e713a7ed
Dhrubajyoti Sadhu <dhsadhu@expediagroup.com>
2024-01-04 23:42:39 +0000

    trying condition

--- templates.tf
+++ templates.tf
@@ -199,7 +199,7 @@ data "template_file" "datadog-agent" {
   vars = {
     region              = var.aws_region
     loggroup            = var.wd_instance_type == "ecs" ? join("", aws_cloudwatch_log_group.waggledance_ecs.*.name) : ""
-    datadog_secret_key = jsondecode(data.aws_secretsmanager_secret_version.datadog_key.secret_string).api_key
+    datadog_secret_key = jsondecode(data.aws_secretsmanager_secret_version.datadog_key[0].secret_string).api_key
     wd_instance_type = var.wd_instance_type
     metrics_port = var.metrics_port
     datadog_agent_version = var.datadog_agent_version
