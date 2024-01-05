a34fde1b713816baa3f8a7912fbfbc653e5baeba
githubjianli <51385385+githubjianli@users.noreply.github.com>
2024-01-04 22:03:06 -0800

    fix: change condition expression for hive-site.xml
    Changed condition expression for hive_site_xml, the older one does not work.

--- templates.tf
+++ templates.tf
@@ -180,7 +180,7 @@ data "template_file" "waggledance" {
     invocationloglevel  = var.enable_invocation_logs ? "debug" : "info"
     server_yaml         = base64encode(data.template_file.server_yaml.rendered)
     federation_yaml     = base64encode(data.template_file.federation_yaml.rendered)
-    hive_site_xml       = var.alluxio_endpoints == [] ? "" : base64encode(data.template_file.hive_site_xml.rendered)
+    hive_site_xml       = length(var.alluxio_endpoints) == 0 ? "" : base64encode(data.template_file.hive_site_xml.rendered)
     bastion_ssh_key_arn = var.bastion_ssh_key_secret_name == "" ? "" : join("", data.aws_secretsmanager_secret.bastion_ssh_key.*.arn)
     docker_auth         = var.docker_registry_auth_secret_name == "" ? "" : format("\"repositoryCredentials\" :{\n \"credentialsParameter\":\"%s\"\n},", join("\",\"", concat(data.aws_secretsmanager_secret.docker_registry.*.arn)))
     tcp_keepalive_time  = var.tcp_keepalive_time
