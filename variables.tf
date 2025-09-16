702adb69abe454d124d4181b82a5941cb4ee426b
janli <janli@expediagroup.com>
2025-09-16 13:33:21 -0700

    fix: use node affinity instead

--- variables.tf
+++ variables.tf
@@ -133,10 +133,32 @@ variable "k8s_dns_config" {
   }
 }
 
-variable "k8s_node_selector_labels" {
-  description = "K8s pods node selector"
-  type        = map(string)
-  default     = {}
+variable "wd_node_affinity" {
+  description = <<EOF
+Adds a list of node affinities for the waggledance pods. For example if you
+have a pool of workers with the following label "pool=metastore" you
+can add an affinity to these workers like this:
+
+wd_node_affinity = [
+  {
+    node_selector_term = [
+      {
+        key      = "pool"
+        operator = "In"
+        values   = ["metastore"]
+      }
+    ]
+  }
+]
+EOF  
+  type = list(object({
+    node_selector_term = list(object({
+      key      = string
+      operator = string
+      values   = list(string)
+    }))
+  }))
+  default = [] # Default to an empty list
 }
 
 variable "k8s_svc_spec" {
