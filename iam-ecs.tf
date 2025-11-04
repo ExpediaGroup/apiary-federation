67eb362d3ed5f5d1da6d756105ebe487ded59d7e
paduin <a-paduin@expediagroup.com>
2025-11-04 16:20:41 +0100

    fixed duplicate resource name

--- iam-ecs.tf
+++ iam-ecs.tf
@@ -106,7 +106,7 @@ resource "aws_iam_role_policy" "waggle_dance_remote_glue_federations_ecs_policy_
   policy = data.aws_iam_policy_document.waggle_dance_remote_glue_federations_policy_write[0].json
 }
 
-resource "aws_iam_role_policy" "waggle_dance_remote_glue_federations_policy_s3_write" {
+resource "aws_iam_role_policy" "waggle_dance_remote_glue_federations_ecs_policy_s3_write" {
   count = var.wd_instance_type == "ecs" && var.s3_glue_tables_bucket != "" && local.glue_enabled ? 1 : 0
   role  = aws_iam_role.waggledance_task[0].name
   name  = "waggle-dance-remote-metastores-glue-s3-write"
